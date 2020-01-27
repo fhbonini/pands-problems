384d20da360095ff3b126fc186463a2f4c3833d4
Fabio Bonini <fhbonini@yahoo.com.br>
2020-01-27 14:08:50 +0000

    add analyse.py

--- analyse.py
+++ analyse.py
@@ -1,12 +1,12 @@
-# this program calculates BMI
+# this program calculates BMI body mass index
 
-altura = float(input("Enter the altura: "))
-peso = float(input("Enter the peso: "))
+height = float(input("Enter the height in cm: "))
+weight = float(input("Enter the weight: "))
 
-bmi = altura * altura / peso
+bmi = (weight) / (height * height)
 
-print("Your BMI is ", bmi)
-if bmi < 0.035:
+print("Your BMI is ", (bmi*10000))
+if bmi < 0.0025:
     print("Thats ok")
 else:
     print("You gonna die")
