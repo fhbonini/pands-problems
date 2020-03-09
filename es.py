56ea97d58dfc9e7348da2ae619a75b7ca371a86c
Fabio Bonini <fhbonini@yahoo.com.br>
2020-03-09 12:12:59 +0000

    import es.py

--- es.py
+++ es.py
@@ -2,6 +2,23 @@
 # programm to count the e's
 
 import sys
+
+def counte():
+    count = 0
+    vocal=["e"]
+    if contents == vocal:
+        count+=1
+    print (count)
+
+
+
 with open(sys.argv[1], 'r') as f:
     contents = f.read()
-print (contents)
+    counte()
+
+#    print(x)
+# print (contents)
+
+
+
+
